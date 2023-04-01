3bb93848c5661d43c2dd9e9d7e4a41925133cdd3
Tomno <tomnoviv@gmail.com>
2023-04-01 16:53:28 +0300

    Completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskInput = document.getElementById("new-task-description");
+  const taskForm = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  taskForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const newTask = taskInput.value.trim();
+
+    if (newTask !== "") {
+      const newTaskItem = document.createElement("li");
+      newTaskItem.textContent = newTask;
+
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "Delete";
+      deleteButton.addEventListener("click", () => {
+        newTaskItem.remove();
+      });
+
+      newTaskItem.appendChild(deleteButton);
+      taskList.appendChild(newTaskItem);
+      taskInput.value = "";
+    }
+  });
 });
